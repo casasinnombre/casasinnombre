d5a5f29305ee4646840a8917c69130c456d74f5a
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2018-11-30 18:25:29 +0000

    Updates

---
+++ precache-manifest.af47ae61f2cb333d7dc8013bbd27ec71.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "73ea79ea85be28f58d8c",
+    "url": "/static/css/main.d2f76ac4.chunk.css"
+  },
+  {
+    "revision": "73ea79ea85be28f58d8c",
+    "url": "/static/js/main.73ea79ea.chunk.js"
+  },
+  {
+    "revision": "a1f2bd7668b1c6d250da",
+    "url": "/static/js/1.a1f2bd76.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4979f9770102a0218912de3b7eee4da2",
+    "url": "/static/media/elliot.4979f977.jpg"
+  },
+  {
+    "revision": "011b41bcb37c106df5b509fb67df3b97",
+    "url": "/static/media/lottie.011b41bc.jpg"
+  },
+  {
+    "revision": "661d726fecb50d03d29a8391ba2078fa",
+    "url": "/static/media/nathan.661d726f.jpg"
+  },
+  {
+    "revision": "e04e3098854278f0f9e944f20809e116",
+    "url": "/static/media/zoe.e04e3098.jpg"
+  },
+  {
+    "revision": "5657752c4071b09a7b91e0813a4a2f2f",
+    "url": "/index.html"
+  }
+];
